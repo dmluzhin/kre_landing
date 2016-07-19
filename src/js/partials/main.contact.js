0a043f68bea8f341f988cfb07327ec2a4961c233
Данила Лужин <d.m.luzhin@gmail.com>
2016-07-19 18:05:45 +0300

    upd angular

--- src/js/partials/main.contact.js
+++ src/js/partials/main.contact.js
@@ -20,36 +20,10 @@
         $scope.isBarkliBrokerFormSended = false;
         $scope.currentTab = 1;
         $scope.params = [];
-        $scope.filter = {
-            rooms: {
-                min: 10,
-                max: 10,
-                currentMin: 10,
-                currentMax: 10,
-                cssClass: '.j-filter-rooms'
-            },
-            floor: {
-                min: 10,
-                max: 10,
-                currentMin: 10,
-                currentMax: 10,
-                cssClass: '.j-filter-floor'
-            },
-            square: {
-                min: 10,
-                max: 10,
-                currentMin: 10,
-                currentMax: 10,
-                cssClass: '.j-filter-square'
-            },
-            price: {
-                min: 10,
-                max: 10,
-                currentMin: 10,
-                currentMax: 10,
-                cssClass: '.j-filter-price'
-            }
-        };
+        $scope.country = [];
+        $scope.commerc = [];
+
+        /*CALLBACK POPUP START*/
 
         $scope.showCallbackPopup = function() {
             if ($scope.isCallbackFormSended) $scope.isCallbackFormSended = false;
@@ -78,7 +52,9 @@
                 });
             }
         };
+        /*CALLBACK POPUP END*/
 
+        /*SIGN POPUP START*/
         $scope.showSignPopup = function() {
             if ($scope.isSignFormSended) $scope.isSignFormSended = false;
             $('.j-popup-sign').arcticmodal();
@@ -106,12 +82,9 @@
                 });
             }
         };
+        /*SIGN POPUP END*/
 
-        $scope.showBarkliBrokerPopup = function() {
-            if ($scope.isBarkliBrokerFormSended) $scope.isBarkliBrokerFormSended = false;
-            $('.j-popup-barkli-broker').arcticmodal();
-        };
-
+        /*SENDING EMAIL TO BROKER START*/
         $scope.sendBarkliBrokerForm = function() {
             if ($scope.barkliBrokerForm.$valid) {
                 $scope.barkliBrokerFormData['subject'] = 'Заявка на квартиру ЖК Андреевский';
@@ -136,109 +109,75 @@
             }
         };
 
-        $http.get(window.apartmentsUrl + window.PID + '/').success(function(data) {
+        /*SENDING EMAIL TO BROKER END*/
 
-            $scope.searchMinMax = {
-                rooms: [],
-                floor: [],
-                square: [],
-                price: []
-            };
-            var flats = data.split('\n');
-            flats.forEach(function(flat, i) {
-                if (flat.length === 0) return;
-                var currentFlat = flat.split(';');
-                $scope.searchMinMax.rooms.push(currentFlat[1]);
-                $scope.searchMinMax.floor.push(currentFlat[2]);
-                $scope.searchMinMax.square.push(currentFlat[3]);
-                $scope.searchMinMax.price.push(currentFlat[5]);
+        function addSpaces(n) {
+            var rx = /(\d+)(\d{3})/;
+            return String(n)
+                .replace(/^\d+/, function (w) {
+                    while (rx.test(w)) {
+                        w = w.replace(rx, '$1 $2');
+                    }
+                    return w;
+                });
+        }
+        $http.get('/city.json').success(function(data) {
+            for (var i=0;i<data.length; i+=1) {
+                var currentFlat = data[i];
                 $scope.params.push({
-                    lot: parseInt(currentFlat[0]),
-                    rooms: parseInt(currentFlat[1]),
-                    floor: parseInt(currentFlat[2]),
-                    square: parseFloat(currentFlat[3]),
-                    section: currentFlat[4],
-                    price: parseFloat(currentFlat[5]),
-                    img: currentFlat[6]
+                    lot: parseInt(currentFlat.id),
+                    estate: currentFlat.estate,
+                    address: currentFlat.address,
+                    rooms: parseInt(currentFlat['nb_rooms']),
+                    floor: parseInt(currentFlat.floor),
+                    square: parseFloat(currentFlat.area),
+                    decoration: currentFlat.decoration,
+                    old_price: addSpaces(parseInt(currentFlat['old_price']/100)),
+                    new_price: addSpaces(parseInt(currentFlat['new_price']/100)),
+                    discount: parseFloat(currentFlat.discount),
+                    img: currentFlat.photo
                 });
-            });
-
-            $scope.flatsSumm = $scope.params.length;
-
-            $scope.slidersInit = function() {
-                for (var prop in $scope.filter) {
-                    $scope.filter[prop].min = $scope.filter[prop].currentMin = $scope.searchMinMax[prop].min();
-                    $scope.filter[prop].max = $scope.filter[prop].currentMax = $scope.searchMinMax[prop].max();
-
-                    $($scope.filter[prop].cssClass).slider({
-                        range: true,
-                        min: $scope.filter[prop].min,
-                        max: $scope.filter[prop].max,
-                        values: [$scope.filter[prop].min, $scope.filter[prop].max]
-                    });
-                }
-
-                $('.j-filter-rooms, .j-filter-floor, .j-filter-square, .j-filter-price').on('slide', function(event, ui) {
-                    var prop = $(this).data('param');
-                    var $scope = angular.element('body').scope();
-                    $scope.$apply(function() {
-                        $scope.filter[prop].currentMin = ui.values[0];
-                        $scope.filter[prop].currentMax = ui.values[1];
-                    });
-                    $('.m-flats__row_odd').removeClass('m-flats__row_odd');
-                    $('.j-flats-row:visible:odd').addClass('m-flats__row_odd');
-                    var $scope = angular.element('body').scope();
-                    $scope.$apply(function() {
-                        $scope.flatsSumm = $('.j-flats-row:visible').length;
-                    });
-                });
-
-            }();
-
+            }
         });
-
-        /*$http.get('/sliders.contact.json').success(function(data) {
-            $scope.hugeSliderData = data;
-        });*/
-
-        /*$scope.flatsReady = function() {
-            $('.j-flats-row:odd').addClass('m-flats__row_odd');
-        };*/
-
-
-        /*$scope.hugeSliderReady = function () {
-            setTimeout(function() {
-                $('.j-huge-slider').slick({
-                    slidesToShow: 1,
-                    speed: 150
+        $http.get('/country.json').success(function(data) {
+            for (var i=0;i<data.length; i+=1) {
+                var currentFlat = data[i];
+                $scope.country.push({
+                    lot: parseInt(currentFlat.id),
+                    estate: currentFlat.estate,
+                    address: currentFlat.address,
+                    direction: currentFlat.direction,
+                    distance: parseInt(currentFlat.distance),
+                    type: currentFlat.type,
+                    land_area: parseFloat(currentFlat['land_area']),
+                    house_area: parseFloat(currentFlat['house_area']),
+                    decoration: currentFlat.decoration,
+                    old_price: addSpaces(parseInt(currentFlat['old_price']/10)),
+                    new_price: addSpaces(parseInt(currentFlat['new_price']/10)),
+                    discount: parseFloat(currentFlat.discount),
+                    img: currentFlat.photo
                 });
-            }, 1);
-        };*/
-
-        /*$scope.showFlatPopup = function(index) {
-            $scope.flatInPopup = $scope.params[index];
-            $('.j-popup-flat').arcticmodal();
-        };
-        $scope.showCountryPopup = function(index) {
-            $scope.flatInPopup = $scope.params[index];
-            $('.j-popup-country').arcticmodal();
-        };
-        $scope.showCommercPopup = function(index) {
-            $scope.flatInPopup = $scope.params[index];
-            $('.j-popup-commerc').arcticmodal();
-        };*/
-
-        $scope.isShowRow = function(index) {
-            return  ($scope.params[index].rooms >=  $scope.filter.rooms.currentMin &&
-                $scope.params[index].rooms <= $scope.filter.rooms.currentMax) &&
-                ($scope.params[index].floor >= $scope.filter.floor.currentMin &&
-                $scope.params[index].floor <= $scope.filter.floor.currentMax) &&
-                ($scope.params[index].square >= $scope.filter.square.currentMin &&
-                $scope.params[index].square <= $scope.filter.square.currentMax) &&
-                ($scope.params[index].price >= $scope.filter.price.currentMin &&
-                $scope.params[index].price <= $scope.filter.price.currentMax);
-        };
-
+            }
+        });
+        $http.get('/commerc.json').success(function(data) {
+            for (var i=0;i<data.length; i+=1) {
+                var currentFlat = data[i];
+                $scope.commerc.push({
+                    lot: parseInt(currentFlat.id),
+                    type: currentFlat.type,
+                    address: currentFlat.address,
+                    district: currentFlat.district,
+                    metro: currentFlat.metro,
+                    distance: currentFlat.distance,
+                    area: parseFloat(currentFlat['area']),
+                    decoration: currentFlat.decoration,
+                    old_price: addSpaces(parseInt(currentFlat['old_price'])),
+                    new_price: addSpaces(parseInt(currentFlat['new_price'])),
+                    discount: parseFloat(currentFlat.discount),
+                    img: currentFlat.photo
+                });
+            }
+        });
     }]);
 
     app.directive("repeatEnd", function () {
