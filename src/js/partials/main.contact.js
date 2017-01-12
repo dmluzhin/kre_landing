f94508094e6a3c69914afbfcdc168d9a048b1821
Данила Лужин <d.m.luzhin@gmail.com>
2017-01-12 17:48:01 +0300

    upd

--- src/js/partials/main.contact.js
+++ src/js/partials/main.contact.js
@@ -24,6 +24,7 @@
         $scope.isOrderFormSended = false;
         $scope.isBarkliBrokerFormSended = false;
         $scope.currentTab = 2;
+        $scope.loadedTabs = [];
         var location = window.location.hash.split('/');
         if (!location[1]) location[1] = 'city';
         if (!location[1]) location[1] = 'country';
@@ -147,149 +148,10 @@
             }
         };
 
-        if($scope.currentTab == 2) {
-            $http.get('city.json').success(function(data) {
-                data.sort(sort);
-                for(var i = 0; i < data.length; i += 1) {
-                    var currentFlat = data[i];
-                    if (currentFlat.suptype == 'city') {
-                        $scope.params.push({
-                            id: currentFlat.id,
-                            city: true,
-                            rooms: (parseInt(currentFlat['nb_rooms']))? (parseInt(currentFlat['nb_rooms'])+' - ком. кв., ') : ' ',
-                            decoration: currentFlat.decoration || '',
-                            floor: (parseInt(currentFlat.floor))? ', '+(parseInt(currentFlat.floor) + '-й этаж') : '',
-                            head: currentFlat.estate,
-                            discount: parseFloat(currentFlat.discount),
-                            square: parseFloat(currentFlat.area),
-                            old_price: addSpaces(parseInt(currentFlat['old_price']/1)),
-                            new_price: addSpaces(parseInt(currentFlat['new_price']/1)),
-                            img: currentFlat.photo,
-                            link: currentFlat.link
-                        });
-                    }
+        $scope.$watch('currentTab', function() {
+            getJson();
+        });
 
-                    $scope.AbsentEmployees.employees = data;
-                    abEmployees.dataLoaded = true;
-
-                    $scope.filter = function(items, attr) {
-                        var result = {};
-                        angular.forEach(items, function(value, key) {
-                            if (value[attr]) {
-                                result[key] = value;
-                            }
-                        });
-                        return result;
-                    };
-                    $scope.test = function(current) {
-                        var center = 0,
-                            blocks = document.querySelectorAll('.b-discount_inner'),
-                            forms = document.querySelectorAll('.b-form');
-
-                        if (parseInt(blocks[current].children.length/2)%2) {
-                            center = parseInt(blocks[current].children.length / 2) + 1;
-                        }
-                        else {
-                            center = parseInt(blocks[current].children.length/2);
-                        }
-                        blocks[current].children[center-1].insertAfter(forms[current]);
-                    }
-                }
-            });
-        } else if ($scope.currentTab == 3) {
-            $http.get('country.json').success(function(data) {
-                data.sort(sort);
-                for(var i = 0; i < data.length; i += 1) {
-                    var currentFlat = data[i];
-                    if (currentFlat.suptype == 'country') {
-                        $scope.params.push({
-                            id: currentFlat.id,
-                            country: true,
-                            desc: currentFlat.direction + ', ' + currentFlat.distance+' км от МКАД',
-                            head: currentFlat.estate,
-                            discount: parseFloat(currentFlat.discount),
-                            square: (parseFloat(currentFlat['land_area']))? +currentFlat['land_area']+' сот., ': '',
-                            housearea: (parseFloat(currentFlat['house_area']))? ''+currentFlat['house_area']+' м²': '',
-                            old_price: addSpaces(parseInt(currentFlat['old_price'])),
-                            new_price: addSpaces(parseInt(currentFlat['new_price'])),
-                            img: currentFlat.photo,
-                            link: currentFlat.link
-                        });
-                    }
-                    $scope.AbsentEmployees.employees = data;
-                    abEmployees.dataLoaded = true;
-
-                    $scope.filter = function(items, attr) {
-                        var result = {};
-                        angular.forEach(items, function(value, key) {
-                            if (value[attr]) {
-                                result[key] = value;
-                            }
-                        });
-                        return result;
-                    };
-                    $scope.test = function(current) {
-                        var center = 0,
-                            blocks = document.querySelectorAll('.b-discount_inner'),
-                            forms = document.querySelectorAll('.b-form');
-
-                        if (parseInt(blocks[current].children.length/2)%2) {
-                            center = parseInt(blocks[current].children.length / 2) + 1;
-                        }
-                        else {
-                            center = parseInt(blocks[current].children.length/2);
-                        }
-                        blocks[current].children[center-1].insertAfter(forms[current]);
-                    }
-                }
-            });
-        } else if ($scope.currentTab == 4) {
-            $http.get('commerc.json').success(function(data) {
-                data.sort(sort);
-                for(var i = 0; i < data.length; i += 1) {
-                    var currentFlat = data[i];
-                if (currentFlat.suptype == 'commerce') {
-                        $scope.params.push({
-                            id: currentFlat.id,
-                            commerce: true,
-                            desc: currentFlat.metro+ ', ' + currentFlat.distance,
-                            head: currentFlat.address || currentFlat.name,
-                            discount: parseFloat(currentFlat.discount),
-                            square: parseFloat(currentFlat.area),
-                            old_price: addSpaces(parseInt(currentFlat['old_price']/1)),
-                            new_price: addSpaces(parseInt(currentFlat['new_price']/1)),
-                            img: currentFlat.photo,
-                            link: currentFlat.link
-                        });
-                    }
-                    $scope.AbsentEmployees.employees = data;
-                    abEmployees.dataLoaded = true;
-
-                    $scope.filter = function(items, attr) {
-                        var result = {};
-                        angular.forEach(items, function(value, key) {
-                            if (value[attr]) {
-                                result[key] = value;
-                            }
-                        });
-                        return result;
-                    };
-                    $scope.test = function(current) {
-                        var center = 0,
-                            blocks = document.querySelectorAll('.b-discount_inner'),
-                            forms = document.querySelectorAll('.b-form');
-
-                        if (parseInt(blocks[current].children.length/2)%2) {
-                            center = parseInt(blocks[current].children.length / 2) + 1;
-                        }
-                        else {
-                            center = parseInt(blocks[current].children.length/2);
-                        }
-                        blocks[current].children[center-1].insertAfter(forms[current]);
-                    }
-                }
-            });
-        }
 
         $scope.showSignPopup = function() {
             if ($scope.isSignFormSended) $scope.isSignFormSended = false;
@@ -313,8 +175,153 @@
         abEmployees.dataLoaded = false;
 
 
+        function getJson() {
+            if ($scope.currentTab == 2 && $scope.loadedTabs.indexOf(2) === -1) {
+                $http.get('http://www.kre.ru/landing/sale/?suptype=city').success(function(data) {
+                    $scope.loadedTabs.push(2);
+                    data.sort(sort);
+                    for(var i = 0; i < data.length; i += 1) {
+                        var currentFlat = data[i];
+                        if (currentFlat.suptype == 'city') {
+                            $scope.params.push({
+                                id: currentFlat.id,
+                                city: true,
+                                rooms: (parseInt(currentFlat['nb_rooms']))? (parseInt(currentFlat['nb_rooms'])+' - ком. кв., ') : ' ',
+                                decoration: currentFlat.decoration || '',
+                                floor: (parseInt(currentFlat.floor))? ', '+(parseInt(currentFlat.floor) + '-й этаж') : '',
+                                head: currentFlat.estate,
+                                discount: parseFloat(currentFlat.discount),
+                                square: parseFloat(currentFlat.area),
+                                old_price: addSpaces(parseInt(currentFlat['old_price']/1)),
+                                new_price: addSpaces(parseInt(currentFlat['new_price']/1)),
+                                img: currentFlat.photo,
+                                link: currentFlat.link
+                            });
+                        }
+                        $scope.AbsentEmployees.employees = data;
+                        abEmployees.dataLoaded = true;
 
+                        $scope.filter = function(items, attr) {
+                            var result = {};
+                            angular.forEach(items, function(value, key) {
+                                if (value[attr]) {
+                                    result[key] = value;
+                                }
+                            });
+                            return result;
+                        };
+                        $scope.test = function(current) {
+                            var center = 0,
+                                blocks = document.querySelectorAll('.b-discount_inner'),
+                                forms = document.querySelectorAll('.b-form');
 
+                            if (parseInt(blocks[current].children.length/2)%2) {
+                                center = parseInt(blocks[current].children.length / 2) + 1;
+                            }
+                            else {
+                                center = parseInt(blocks[current].children.length/2);
+                            }
+                            blocks[current].children[center-1].insertAfter(forms[current]);
+                        }
+                    }
+                });
+            } else if ($scope.currentTab == 3 && $scope.loadedTabs.indexOf(3) === -1) {
+                $http.get('http://www.kre.ru/landing/sale/?suptype=country').success(function(data) {
+                    $scope.loadedTabs.push(3);
+                    data.sort(sort);
+                    for(var i = 0; i < data.length; i += 1) {
+                        var currentFlat = data[i];
+                        if (currentFlat.suptype == 'country') {
+                            $scope.params.push({
+                                id: currentFlat.id,
+                                country: true,
+                                desc: currentFlat.direction + ', ' + currentFlat.distance+' км от МКАД',
+                                head: currentFlat.estate,
+                                discount: parseFloat(currentFlat.discount),
+                                square: (parseFloat(currentFlat['land_area']))? +currentFlat['land_area']+' сот., ': '',
+                                housearea: (parseFloat(currentFlat['house_area']))? ''+currentFlat['house_area']+' м²': '',
+                                old_price: addSpaces(parseInt(currentFlat['old_price'])),
+                                new_price: addSpaces(parseInt(currentFlat['new_price'])),
+                                img: currentFlat.photo,
+                                link: currentFlat.link
+                            });
+                        }
+                        $scope.AbsentEmployees.employees = data;
+                        abEmployees.dataLoaded = true;
+
+                        $scope.filter = function(items, attr) {
+                            var result = {};
+                            angular.forEach(items, function(value, key) {
+                                if (value[attr]) {
+                                    result[key] = value;
+                                }
+                            });
+                            return result;
+                        };
+                        $scope.test = function(current) {
+                            var center = 0,
+                                blocks = document.querySelectorAll('.b-discount_inner'),
+                                forms = document.querySelectorAll('.b-form');
+
+                            if (parseInt(blocks[current].children.length/2)%2) {
+                                center = parseInt(blocks[current].children.length / 2) + 1;
+                            }
+                            else {
+                                center = parseInt(blocks[current].children.length/2);
+                            }
+                            blocks[current].children[center-1].insertAfter(forms[current]);
+                        }
+                    }
+                });
+            } else if ($scope.currentTab == 4 && $scope.loadedTabs.indexOf(4) === -1) {
+                $http.get('http://www.kre.ru/landing/sale/?suptype=commercial').success(function(data) {
+                    $scope.loadedTabs.push(4);
+                    data.sort(sort);
+                    for(var i = 0; i < data.length; i += 1) {
+                        var currentFlat = data[i];
+                        if (currentFlat.suptype == 'commerce') {
+                            $scope.params.push({
+                                id: currentFlat.id,
+                                commerce: true,
+                                desc: currentFlat.metro+ ', ' + currentFlat.distance,
+                                head: currentFlat.address || currentFlat.name,
+                                discount: parseFloat(currentFlat.discount),
+                                square: parseFloat(currentFlat.area),
+                                old_price: addSpaces(parseInt(currentFlat['old_price']/1)),
+                                new_price: addSpaces(parseInt(currentFlat['new_price']/1)),
+                                img: currentFlat.photo,
+                                link: currentFlat.link
+                            });
+                        }
+                        $scope.AbsentEmployees.employees = data;
+                        abEmployees.dataLoaded = true;
+
+                        $scope.filter = function(items, attr) {
+                            var result = {};
+                            angular.forEach(items, function(value, key) {
+                                if (value[attr]) {
+                                    result[key] = value;
+                                }
+                            });
+                            return result;
+                        };
+                        $scope.test = function(current) {
+                            var center = 0,
+                                blocks = document.querySelectorAll('.b-discount_inner'),
+                                forms = document.querySelectorAll('.b-form');
+
+                            if (parseInt(blocks[current].children.length/2)%2) {
+                                center = parseInt(blocks[current].children.length / 2) + 1;
+                            }
+                            else {
+                                center = parseInt(blocks[current].children.length/2);
+                            }
+                            blocks[current].children[center-1].insertAfter(forms[current]);
+                        }
+                    }
+                });
+            }
+        }
     }]);
     app.directive("repeatEnd", function () {
         return {
